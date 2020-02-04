3cdd615eb72c1a339602b2f73217f87d816039af
Quinten Clause <quinten.clause@studioemma.eu>
2020-02-04 13:42:15 +0100

    click distance from center

--- clips/20200202.js
+++ clips/20200202.js
@@ -1,4 +1,12 @@
 let square;
+let listener = (e) => {
+    let dx = width / 2 - e.x;
+    let dy = height / 2 - e.y;
+    let d = Math.sqrt( dx * dx + dy * dy );
+    if (d < 20) {
+        nextModule();
+    }
+};
 
 export const add = () => {
     canvas.style.backgroundColor = "#8e44ad";
@@ -8,16 +16,14 @@ export const add = () => {
         this.y = height / 2;
         this.rotation = Math.sin(-time / 600) * Math.PI;
         context.fillStyle = '#f39c12';
-    context.strokeStyle = '#f39c12';
+        context.strokeStyle = '#f39c12';
         context.fillRect(-16, -16, 32, 32);
         context.strokeRect(128, -16, 32, 32);
     };
-    events.once('pointerup', (e) => {
-        console.log(e);
-        nextModule();
-    });
+    events.on('pointerup', listener);
 };
 
 export const remove = () => {
+    events.off('pointerup', listener);
     removeClip(square);
 };
