c9bfbd3570cdd24ed6dc457f5dd0023457874fd0
Quinten Clause <quinten.clause@studioemma.eu>
2020-02-04 15:36:21 +0100

    Adjust click target

--- clips/20200203.js
+++ clips/20200203.js
@@ -1,4 +1,14 @@
 let square;
+let sx = 0;
+let sy = 0;
+let listener = (e) => {
+    let dx = sx - e.x;
+    let dy = sy - e.y;
+    let d = Math.sqrt( dx * dx + dy * dy );
+    if (d < 20) {
+        nextModule();
+    }
+};
 
 export const add = () => {
     canvas.style.backgroundColor = "#2c3e50";
@@ -7,16 +17,17 @@ export const add = () => {
         this.x = width / 2;
         this.y = height / 2;
         this.rotation = Math.sin(-time / 600) * Math.PI;
+        sx = this.x + 134 * Math.cos(this.rotation);
+        sy = this.y + 134 * Math.sin(this.rotation);
         context.fillStyle = '#869496';
-    context.strokeStyle = '#869496';
+        context.strokeStyle = '#869496';
         context.strokeRect(-16, -16, 32, 32);
         context.fillRect(128, -16, 32, 32);
     };
-    events.once('pointerup', (e) => {
-        nextModule();
-    });
+    events.on('pointerup', listener);
 };
 
 export const remove = () => {
+    events.off('pointerup', listener);
     removeClip(square);
 };
