0fa0b4c30f3db91b27f81a735a0c9167ef72339b
Quinten Clause <quintenclause@hotmail.com>
2021-02-13 21:50:37 +0100

    Clean code

--- clips/nodes.js
+++ clips/nodes.js
@@ -1,15 +1,5 @@
 import colors from '../lib/color.js';
 
-let square;
-let listener = (e) => {
-    let dx = width / 2 - e.x;
-    let dy = height * 2 / 3 - e.y;
-    let d = Math.sqrt( dx * dx + dy * dy );
-    if (d < 48) {
-        nextModule();
-    }
-};
-
 let bigSquares = [];
 
 export const add = () => {
@@ -77,29 +67,9 @@ export const add = () => {
         };
         bigSquares.push(bigSquare);
     }
-
-    /*
-    square = addClip();
-    square.draw = function (time) {
-        this.x = width / 2;
-        this.y = height * 2 / 3;
-        context.fillStyle = '#ecf0f1';
-        context.fillRect(-16, -16, 32, 32);
-        context.strokeStyle = '#ecf0f1';
-        let pulse = time % 2000;
-        pulse = pulse * pulse / 4000000;
-        context.globalAlpha = 1 - pulse;
-        pulse = 16 + pulse * 24;
-        context.strokeRect(-pulse, -pulse, pulse * 2, pulse * 2);
-    };
-
-    events.on('pointerup', listener);
-    */
 };
 
 export const remove = () => {
-    //events.off('pointerup', listener);
-    //removeClip(square);
     bigSquares.forEach((bigSquare) => {
         removeClip(bigSquare);
     });
