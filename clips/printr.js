59a72d54d1073d1fd8a2e5b9b0bba76990fa4ac6
Quinten Clause <quintenclause@hotmail.com>
2021-02-21 21:05:25 +0100

    Tweak printr

--- clips/printr.js
+++ clips/printr.js
@@ -9,7 +9,20 @@ export const add = () => {
     let strokeColor = (!invert) ? color.pale.hsl : color.dark.hsl;
     canvas.style.backgroundColor = bgColor;
 
-    myClip = addClip();
+    let printb = (prints, s) => {
+        ctx.fillStyle = strokeColor;
+        prints.forEach((print) => {
+            ctx.fillRect(print.x - 10 - s * 20, print.y - 187, 20 + s * 40, 374);
+            ctx.fillRect(print.x - 30 - s * 20, print.y - 30 - s * 20, 60 + s * 40, 60 + s * 40);
+        });
+        ctx.fillStyle = bgColor;
+        prints.forEach((print) => {
+            ctx.fillRect(print.x - 10 - s * 20 + 2, print.y - 188, 20 + s * 40 - 4, 376);
+            ctx.fillRect(print.x - 30 - s * 20 + 2, print.y - 30 - s * 20 + 2, 60 + s * 40 - 4, 60 + s * 40 - 4);
+        });
+    };
+
+    myClip = addClip({unshift: true});
     let nFramesC = 1024;
     if (nFrames > -1) {
         nFrames = 128;
@@ -21,7 +34,7 @@ export const add = () => {
 
     for (let p = 0; p < nPrints; p++) {
 
-        let extraL = 70 + Math.floor(Math.random() * 8) * 20;
+        let extraL = 250 + Math.floor(Math.random() * 8) * 50;
         let L = height + extraL * 2;
         let dir = (Math.round(Math.random())) ? 1 : -1;
         let speed = L / nFramesC * dir;
@@ -36,6 +49,8 @@ export const add = () => {
 
     myClip.draw = function (time) {
         ctx.save();
+        ctx.fillStyle = bgColor;
+        ctx.fillRect(0, 0, width, height);
         if (nFrames === -1) {
             ctx.scale(2, 2);
         }
@@ -51,35 +66,41 @@ export const add = () => {
         ctx.fillStyle = strokeColor;
         prints.forEach((print) => {
             ctx.fillRect(print.x - 50, 0, 100, height);
-            ctx.fillRect(print.x - 66, print.y - 66, 132, 132);
+            //ctx.fillRect(print.x - 66, print.y - 66, 132, 132);
         });
         ctx.fillStyle = bgColor;
         prints.forEach((print) => {
             ctx.fillRect(print.x - 48, 0, 96, height);
-            ctx.fillRect(print.x - 64, print.y - 64, 128, 128);
+            //ctx.fillRect(print.x - 64, print.y - 64, 128, 128);
         });
         ctx.fillStyle = strokeColor;
         prints.forEach((print) => {
-            //print.y += print.speed;
             ctx.fillRect(print.x - 30, 0, 60, height);
-            ctx.fillRect(print.x - 50, print.y - 50, 100, 100);
+            //ctx.fillRect(print.x - 50, print.y - 50, 100, 100);
         });
         ctx.fillStyle = bgColor;
         prints.forEach((print) => {
             ctx.fillRect(print.x - 28, 0, 56, height);
-            ctx.fillRect(print.x - 48, print.y - 48, 96, 96);
+            //ctx.fillRect(print.x - 48, print.y - 48, 96, 96);
         });
         ctx.fillStyle = strokeColor;
         prints.forEach((print) => {
-            //print.y += print.speed;
             ctx.fillRect(print.x - 10, 0, 20, height);
-            ctx.fillRect(print.x - 30, print.y - 30, 60, 60);
+            //ctx.fillRect(print.x - 30, print.y - 30, 60, 60);
         });
         ctx.fillStyle = bgColor;
         prints.forEach((print) => {
             ctx.fillRect(print.x - 8, 0, 16, height);
-            ctx.fillRect(print.x - 28, print.y - 28, 56, 56);
+            //ctx.fillRect(print.x - 28, print.y - 28, 56, 56);
         });
+        printb(prints, 7);
+        printb(prints, 6);
+        printb(prints, 5.1);
+        printb(prints, 4.1);
+        printb(prints, 3.1);
+        printb(prints, 2);
+        printb(prints, 1);
+        printb(prints, 0);
         ctx.restore();
     };
 };
