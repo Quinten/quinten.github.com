6d4fb623d6dbbde19770c29b43f38544ecba485c
Quinten Clause <quintenclause@hotmail.com>
2021-02-21 19:03:52 +0100

    Update printr

--- clips/printr.js
+++ clips/printr.js
@@ -10,60 +10,77 @@ export const add = () => {
     canvas.style.backgroundColor = bgColor;
 
     myClip = addClip();
+    let nFramesC = 1024;
+    if (nFrames > -1) {
+        nFrames = 128;
+        nFramesC = nFrames;
+    }
 
-    let nPrints = Math.floor(Math.hypot(window.innerWidth * 2, height) / 32), prints = [];
+    let nPrints = Math.max(6, Math.ceil(window.innerWidth / 120) + 1);
+    let prints = [];
 
     for (let p = 0; p < nPrints; p++) {
 
-        let dir = Math.random() * Math.PI;
-        let speed = 1 + Math.random();
-
+        let extraL = 70 + Math.floor(Math.random() * 8) * 20;
+        let L = height + extraL * 2;
+        let dir = (Math.round(Math.random())) ? 1 : -1;
+        let speed = L / nFramesC * dir;
+        let x = p * 120;
+        let y = - extraL + Math.floor (Math.random() * L);
         let print = {
-            x: Math.floor(Math.random() * (window.innerWidth * 2 + 64)) - 32,
-            y: Math.floor(Math.random() * (height + 64)) - 32,
-            w: (32 + (Math.floor(Math.random() * 24) * 16)),
-            h: (32 + (Math.floor(Math.random() * 24) * 16)),
-            vx: Math.cos(dir) * speed,
-            vy: Math.sin(dir) * speed
+            x, y, extraL, L, dir, speed
         };
 
         prints.push(print);
     }
 
     myClip.draw = function (time) {
-
-        let width = window.innerWidth * 2;
-
-        context.fillStyle = strokeColor;
+        ctx.save();
+        if (nFrames === -1) {
+            ctx.scale(2, 2);
+        }
         prints.forEach((print) => {
-            print.x += print.vx;
-            print.y += print.vy;
-            if (print.x + 12 + print.w < 0) {
-                print.x += width + 24 + print.w;
+            print.y += print.speed;
+            if (print.y < -print.extraL) {
+                print.y += print.L;
             }
-            if (print.y + 12 + print.h < 0) {
-                print.y += height + 24 + print.h;
+            if (print.y > height + print.extraL) {
+                print.y -= print.L;
             }
-            if (print.x - 12 > width) {
-                print.x -= width + 24 + print.w;
-            }
-            if (print.y - 12 > height) {
-                print.y -= height + 24 + print.h;
-            }
-            context.fillRect(print.x, print.y, print.w, print.h);
         });
-        context.fillStyle = bgColor;
+        ctx.fillStyle = strokeColor;
         prints.forEach((print) => {
-            context.fillRect(print.x + 2, print.y + 2, print.w - 4, print.h - 4);
+            ctx.fillRect(print.x - 50, 0, 100, height);
+            ctx.fillRect(print.x - 66, print.y - 66, 132, 132);
         });
-        context.fillStyle = strokeColor;
+        ctx.fillStyle = bgColor;
         prints.forEach((print) => {
-            context.fillRect(print.x + 12, print.y + 12, print.w - 24, print.h - 24);
+            ctx.fillRect(print.x - 48, 0, 96, height);
+            ctx.fillRect(print.x - 64, print.y - 64, 128, 128);
         });
-        context.fillStyle = bgColor;
+        ctx.fillStyle = strokeColor;
         prints.forEach((print) => {
-            context.fillRect(print.x + 14, print.y + 14, print.w - 28, print.h - 28);
+            //print.y += print.speed;
+            ctx.fillRect(print.x - 30, 0, 60, height);
+            ctx.fillRect(print.x - 50, print.y - 50, 100, 100);
         });
+        ctx.fillStyle = bgColor;
+        prints.forEach((print) => {
+            ctx.fillRect(print.x - 28, 0, 56, height);
+            ctx.fillRect(print.x - 48, print.y - 48, 96, 96);
+        });
+        ctx.fillStyle = strokeColor;
+        prints.forEach((print) => {
+            //print.y += print.speed;
+            ctx.fillRect(print.x - 10, 0, 20, height);
+            ctx.fillRect(print.x - 30, print.y - 30, 60, 60);
+        });
+        ctx.fillStyle = bgColor;
+        prints.forEach((print) => {
+            ctx.fillRect(print.x - 8, 0, 16, height);
+            ctx.fillRect(print.x - 28, print.y - 28, 56, 56);
+        });
+        ctx.restore();
     };
 };
 
