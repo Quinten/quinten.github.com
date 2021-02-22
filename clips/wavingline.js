e7234f7528adb9521e20d7532d92b6f0674ee4ad
Quinten Clause <quintenclause@hotmail.com>
2021-02-22 20:26:52 +0100

    Make wavingline gifable

--- clips/wavingline.js
+++ clips/wavingline.js
@@ -8,18 +8,24 @@ export const add = () => {
     let strokeColor = (color.invert) ? color.lite.hsl : color.base.hsl;
     canvas.style.backgroundColor = bgColor;
 
+    if (nFrames > -1) {
+        nFrames = 98;
+        startFrame = 4;
+    }
+
     bigSquares = [];
     for (let b = 0; b < 16; b++) {
         let bigSquare = addClip();
         bigSquare.phase = b / 15;
         bigSquare.draw = (time) => {
+            time = (nFrames > -1) ? frameIndex * 40 : time;
             bigSquare.x = width / 2;
             bigSquare.y = height * bigSquare.phase;
             bigSquare.rotation = Math.sin((time + 1200 * bigSquare.phase) / 600) * Math.PI;
             bigSquare.rotation = Math.max(bigSquare.rotation, - Math.PI / 2);
             bigSquare.rotation = Math.min(bigSquare.rotation, Math.PI / 2);
             context.fillStyle = strokeColor;
-            context.fillRect(-height / 30, - 8, height / 15, 16);
+            context.fillRect(-height / 30, - 4, height / 15, 8);
         };
         bigSquares.push(bigSquare);
     }
