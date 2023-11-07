29ab57bacb609bcb027d617c1e220a902ac4a403
Quinten Clause <quintenclause@hotmail.com>
2023-11-07 22:47:55 +0100

    Add timeline logic

--- transmissions.js
+++ transmissions.js
@@ -2,6 +2,31 @@ fetch('./transmissions.json').then((response) => {
     return response.json();
 }).then((data) => {
     modules = data;
-    chaosMode = true;
-    shuffleModule();
+    modules.sort();
+    import('./lib/timeline.js').then((module) => {
+        let timeline = module.default;
+        let currentClip = null;
+        let previousClip = null;
+        let updateClip = async () => {
+            let TOLength = 0;
+            if (previousClip != null) {
+                TOLength = 8000;
+            }
+            let newClip = timeline.getPresentElement(modules);
+            if (newClip != currentClip) {
+                previousClip = currentClip;
+                currentClip = newClip;
+                let newModule = await import('./clips/' + currentClip);
+                while (clips.length > 0) {
+                    let oldClip = clips.pop();
+                    if (oldClip.remove) {
+                        oldClip.remove();
+                    }
+                }
+                newModule.add();
+            }
+            createTimeout(updateClip, TOLength);
+        };
+        updateClip();
+    });
 });
