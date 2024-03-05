1eceef69411d4e5b1de6abe18eeacecb7a72ea9c
Quinten Clause <quintenclause@hotmail.com>
2024-03-05 16:36:39 +0100

    Savepoint

--- blog/skiptomenu.js
+++ blog/skiptomenu.js
@@ -1,6 +1,6 @@
 let skipToMenu = e => {
     e.preventDefault();
-    menu.scrollIntoView({'behavior': 'smooth'});
+    menu.scrollIntoView({behavior: 'smooth'});
     firstmenulink.focus();
 };
 document.querySelector('[href="#menu"]').addEventListener('click', skipToMenu);
