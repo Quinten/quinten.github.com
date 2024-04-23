844ceb765e073602fe98edecc75424437614a902
Quinten Clause <quintenclause@hotmail.com>
2024-04-23 19:28:56 +0200

    Fix zooming

--- pad/index.js
+++ pad/index.js
@@ -273,6 +273,7 @@ document.querySelectorAll('.custom-touch').forEach(container => {
 
     let currentPad = undefined;
 
+    let nTaps = 0;
     let handleTaps = e => {
         nTaps++;
         if (nTaps === 1) {
@@ -342,8 +343,6 @@ document.querySelectorAll('.custom-touch').forEach(container => {
         }
     };
 
-    // two finger panning and pinch zooming
-
     let inner = container.querySelector('.custom-touch-inner');
     let left = 0;
     let top = 0;
@@ -352,23 +351,39 @@ document.querySelectorAll('.custom-touch').forEach(container => {
         inner.style.top = (top + y) + 'px';
     };
     updatePosition(0, 0);
-    let zoom = inner.clientWidth / window.innerWidth;
-    let lastWidth = inner.clientWidth;
+    let startWidth = inner.clientWidth;
     let updateScale = scale => {
-        inner.style.width = (window.innerWidth * zoom * scale) + 'px';
-        return inner.clientWidth - lastWidth;
+        inner.style.width = (startWidth * scale) + 'px';
     };
     updateScale(1);
+    let spaceDown = false;
+    let panStarted = false;
+    let stopPanning = () => {
+        if (panStarted) {
+            top = Number(inner.style.top.replace('px', ''));
+            left = Number(inner.style.left.replace('px', ''));
+            panStarted = false;
+            container.style.cursor = spaceDown ? 'grab' : 'auto';
+        }
+    };
+    let stopZooming = () => {
+        top = Number(inner.style.top.replace('px', ''));
+        left = Number(inner.style.left.replace('px', ''));
+        startWidth = inner.clientWidth;
+    };
+    let resetZooming = () => {
+        left = 0;
+        top = 0;
+        updatePosition(0, 0);
+        startWidth = window.innerWidth;
+        updateScale(1);
+    };
 
     let oldOnViewOpen = window.onViewOpen;
     window.onViewOpen = e => {
         oldOnViewOpen(e);
         if (e.view === 'editor') {
-            left = 0;
-            top = 0;
-            updatePosition(0, 0);
-            zoom = 1;
-            updateScale(1);
+            resetZooming();
         } else {
             deselectAllElements();
         }
@@ -378,11 +393,9 @@ document.querySelectorAll('.custom-touch').forEach(container => {
     let touchId2 = undefined;
     let touch1 = undefined;
     let touch2 = undefined;
-    let lastX = undefined;
-    let lastY = undefined;
-    let lastScale = undefined;
-
-    let nTaps = 0;
+    let startX = window.innerWidth / 2;
+    let startY = window.innerHeight / 2;
+    let startScale = undefined;
 
     container.addEventListener('touchstart', e => {
         e.preventDefault();
@@ -392,10 +405,9 @@ document.querySelectorAll('.custom-touch').forEach(container => {
             touchId2 = touches[1].identifier;
             touch1 = touches[0];
             touch2 = touches[1];
-            lastX = touch1.screenX + (touch2.screenX - touch1.screenX) / 2;
-            lastY = touch1.screenY + (touch2.screenY - touch1.screenY) / 2;
-            lastScale = Math.hypot(touch2.screenX - touch1.screenX, touch2.screenY - touch1.screenY);
-            lastWidth = inner.clientWidth;
+            startX = touch1.screenX + (touch2.screenX - touch1.screenX) / 2;
+            startY = touch1.screenY + (touch2.screenY - touch1.screenY) / 2;
+            startScale = Math.hypot(touch2.screenX - touch1.screenX, touch2.screenY - touch1.screenY);
         }
         if (touches.length === 1) {
             handleTaps(e);  
@@ -413,13 +425,16 @@ document.querySelectorAll('.custom-touch').forEach(container => {
                     touch2 = touch;
                 }
             }
-            let scale = Math.hypot(touch2.screenX - touch1.screenX, touch2.screenY - touch1.screenY);
-            let dScale = scale / lastScale;
-            let dW = updateScale(dScale);
             let x = touch1.screenX + (touch2.screenX - touch1.screenX) / 2;
             let y = touch1.screenY + (touch2.screenY - touch1.screenY) / 2;
-            let dX = x - lastX - dW / 2;
-            let dY = y - lastY - dW / 2;
+            let scale = Math.hypot(touch2.screenX - touch1.screenX, touch2.screenY - touch1.screenY);
+
+            let dScale = scale / startScale;
+            updateScale(dScale);
+            let dX = (x - startX);
+            let dY = (y - startY);
+            dX = dX + (left - startX) * (dScale - 1);
+            dY = dY + (top - startY) * (dScale - 1);
             updatePosition(dX, dY);
         }
     });
@@ -431,16 +446,12 @@ document.querySelectorAll('.custom-touch').forEach(container => {
             if (touch.identifier === touchId1 || touch.identifier === touchId2) {
                 touchId1 = undefined;
                 touchId2 = undefined;
-                left = Number(inner.style.left.replace('px', ''));
-                top = Number(inner.style.top.replace('px', ''));
-                zoom = inner.clientWidth / window.innerWidth;
+                stopZooming();
             }
         }
     });
 
     // desktop
-    let spaceDown = false;
-    let panStarted = false;
     window.addEventListener('keydown', e => {
         if (e.keyCode === 32) {
             spaceDown = true;
@@ -456,8 +467,8 @@ document.querySelectorAll('.custom-touch').forEach(container => {
     container.addEventListener('mousedown', e => {
         e.preventDefault();
         if (spaceDown) {
-            lastX = e.screenX;
-            lastY = e.screenY;
+            startX = e.screenX;
+            startY = e.screenY;
             panStarted = true;
             container.style.cursor = 'grabbing';
         } else {
@@ -467,28 +478,19 @@ document.querySelectorAll('.custom-touch').forEach(container => {
     container.addEventListener('mousemove', e => {
         e.preventDefault();
         if (spaceDown && panStarted) {
-            let dX = e.screenX - lastX;
-            let dY = e.screenY - lastY;
+            let dX = e.screenX - startX;
+            let dY = e.screenY - startY;
             updatePosition(dX, dY);
+        } else {
+            startX = e.screenX;
+            startY = e.screenY;
         }
     });
     container.addEventListener('mouseup', e => {
-        e.preventDefault();
-        if (panStarted) {
-            top = Number(inner.style.top.replace('px', ''));
-            left = Number(inner.style.left.replace('px', ''));
-            panStarted = false;
-            container.style.cursor = spaceDown ? 'grab' : 'auto';
-        }
+        stopPanning();
     });
     container.addEventListener('mouseleave', e => {
-        e.preventDefault();
-        if (panStarted) {
-            top = Number(inner.style.top.replace('px', ''));
-            left = Number(inner.style.left.replace('px', ''));
-            panStarted = false;
-            container.style.cursor = spaceDown ? 'grab' : 'auto';
-        }
+        stopPanning();
     });
     let wheelTO = undefined;
     container.addEventListener('wheel', e => {
@@ -497,16 +499,16 @@ document.querySelectorAll('.custom-touch').forEach(container => {
             if (wheelTO) {
                 clearTimeout(wheelTO);
             } else {
-                lastScale = 1;
-                lastWidth = inner.clientWidth;
+                startScale = 1;
             }
-            lastScale -= e.deltaY / 100;
-            let dW = updateScale(lastScale);
-            updatePosition(-dW / 2, -dW / 2);
+            startScale -= e.deltaY / 1000;
+            startScale = Math.max(0.1, startScale);
+            updateScale(startScale);
+            let dX = (left - startX) * (startScale - 1);
+            let dY = (top - startY) * (startScale - 1);
+            updatePosition(dX, dY);
             wheelTO = setTimeout(() => {
-                left = Number(inner.style.left.replace('px', ''));
-                top = Number(inner.style.top.replace('px', ''));
-                zoom = inner.clientWidth / window.innerWidth;
+                stopZooming();
                 wheelTO = undefined;
             }, 100);
         }
